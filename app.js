29be37142e8de7619fe048d9ad1701a9b8bf8a84
Alain Leglise <alain@yuumi-soft.fr>
2022-12-04 18:00:59 +0000

    Correction du message envoye

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 7000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Application node JS basique</h1>');
+    res.send('<h1>Application node JS basique sur server 191.96.1.238</h1>');
 });
 
 app.listen(port, () => {
